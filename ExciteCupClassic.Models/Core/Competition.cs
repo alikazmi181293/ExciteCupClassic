240d7cfcb11509fb75e2c11298350e64213a412b
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2025-04-16 22:31:35 +0300

    Added Competition Entity

---
+++ ExciteCupClassic.Models/Core/Competition.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExciteCupClassic.Models.Core;
+
+[Table("competitions")]
+public class Competition
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Fee { get; set; }
+    public string Venue { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+}
