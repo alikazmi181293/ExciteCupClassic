941e6813a1b5a1e7ffe2e16231636a720df0cfaa
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2025-04-17 21:44:03 +0300

    Add DbContext in the project

---
+++ ExciteCupClassic.Database/ExciteCupDbContext/ExciteCupClassicDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ExciteCupClassic.Models.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExciteCupClassic.Database.ExciteCupDbContext;
+
+public class ExciteCupClassicDbContext : DbContext
+{
+    public ExciteCupClassicDbContext(DbContextOptions<ExciteCupClassicDbContext> options) : base(options) { }
+
+    public DbSet<Competition> Competitions { get; set; }
+}
+
