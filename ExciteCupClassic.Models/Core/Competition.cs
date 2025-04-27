e23b73477d74921a791cea8fb384eefd8e856a6c
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2025-04-27 11:24:11 +0300

    Fixed the sytax error in the competitions class

--- ExciteCupClassic.Models/Core/Competition.cs
+++ ExciteCupClassic.Models/Core/Competition.cs
@@ -8,6 +8,6 @@ public class Competition
     public long Id { get; set; }
     public required string Name { get; set; }
     public required int Fee { get; set; }
-    public required string Venue { get; set; };
+    public required string Venue { get; set; }
     public required DateTime Date { get; set; }
 }
