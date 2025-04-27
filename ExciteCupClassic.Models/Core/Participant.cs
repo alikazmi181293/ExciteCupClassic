3d3c1825e7754381be4c5ce3a5ad87264df0ddbc
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2025-04-27 11:21:21 +0300

    Created Participant entrity

---
+++ ExciteCupClassic.Models/Core/Participant.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExciteCupClassic.Models.Core;
+
+[Table("participants")]
+public class Participant
+{
+    public long Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string CellPhone { get; set; }
+    public required string Email {  get; set; }
+}
